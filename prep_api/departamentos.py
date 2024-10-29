7b35a65254f34eeb5e0ba6c77759f9eba0421780
PapelConPera <lionel.copajira@gmail.com>
2024-10-29 12:03:55 -0300

    armando el detalle

--- prep_api/departamentos.py
+++ prep_api/departamentos.py
@@ -11,7 +11,31 @@ bp = Blueprint('departamentos', __name__,url_prefix='/departamentos')
 def index():
     db = get_db()
     lista_departamentos = db.execute(
-          """SELECT department_name as departamentos
+          """SELECT department_name as departamentos, department_id
              FROM departments;"""
     ).fetchall()
     return render_template('departamento.html', departamentos=lista_departamentos)
+
+@bp.route('/detalle/<int:id>/')
+def detalle(id):
+    db = get_db()
+    consulta1 = """
+          SELECT department_name as departamentos, department_id
+            FROM departments
+            WHERE department_id = ?"""
+
+    resultado = db.execute(consulta1, (id,))
+    lista_departamentos = resultado.fetchone()
+
+    consulta2 = """  
+           SELECT first_name, last_name as apellido, employee_id
+           FROM employees
+           WHERE department_id = ?"""
+    
+    resultado2 = db.execute(consulta2, (id,))
+    lista_empleados = resultado2.fetchall()
+    
+
+    pagina = render_template('detalleDeparta.html', departamento=lista_departamentos, empleado = lista_empleados)
+
+    return pagina
