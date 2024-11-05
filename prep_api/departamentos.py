6c1d0487fad31d64af5551a5ce3d00b550bcebbd
PapelConPera <lionel.copajira@gmail.com>
2024-11-05 12:12:08 -0300

    armando los links y en proceso

--- prep_api/departamentos.py
+++ prep_api/departamentos.py
@@ -12,7 +12,7 @@ def index():
     db = get_db()
     lista_departamentos = db.execute(
           """SELECT department_name as departamentos, department_id
-             FROM departments;"""
+             FROM departments"""
     ).fetchall()
     return render_template('departamento.html', departamentos=lista_departamentos)
 
@@ -20,17 +20,17 @@ def index():
 def detalle(id):
     db = get_db()
     consulta1 = """
-          SELECT department_name as departamentos, department_id
-            FROM departments
-            WHERE department_id = ?"""
+        SELECT department_name as departamentos, department_id
+        FROM departments
+        WHERE department_id = ?"""
 
     resultado = db.execute(consulta1, (id,))
     lista_departamentos = resultado.fetchone()
 
     consulta2 = """  
-           SELECT first_name, last_name as apellido, employee_id
-           FROM employees
-           WHERE department_id = ?"""
+        ELECT first_name as nombre, last_name as apellido, employee_id
+        FROM employees
+        WHERE department_id = ?"""
     
     resultado2 = db.execute(consulta2, (id,))
     lista_empleados = resultado2.fetchall()
--- prep_api/empleados.py
+++ prep_api/empleados.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
+
 from werkzeug.exceptions import abort
 
 from prep_api.db import get_db
@@ -11,7 +12,23 @@ bp = Blueprint('empleados', __name__,url_prefix='/empleados')
 def index():
     db = get_db()
     lista_empleado = db.execute(
-          """SELECT first_name as nombre, last_name as apellido
+          """SELECT first_name, last_name, employee_id
              FROM employees;"""
     ).fetchall()
     return render_template('empleados.html', empleado=lista_empleado)
+
+@bp.route('/<int:id>/')
+def detalle(id):
+    db = get_db()
+    consulta = """
+            SELECT first_name, last_name, employee_id
+            FROM employees
+            WHERE employee_id = ?"""
+
+    resultado = db.execute(consulta, (id,))
+    lista_empleado= resultado.fetchall()
+
+
+    pagina = render_template('detalleEmpleados.html', empleado = lista_empleado)
+
+    return pagina
