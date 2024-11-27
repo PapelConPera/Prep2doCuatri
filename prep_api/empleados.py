48b4787d54fdf94bf260d02baa6a1dda68b3fe68
PapelConPera <lionel.copajira@gmail.com>
2024-11-27 15:22:49 -0300

    adjunte la informacion de los trabajos de los empleados en la tabla de detalle empleado

--- prep_api/empleados.py
+++ prep_api/empleados.py
@@ -22,9 +22,10 @@ def detalle(id):
     db = get_db()
     #agregar los salarios y los emails
     consulta = """
-        SELECT e.first_name, e.last_name, e.employee_id, e.department_id, e.phone_number, e.salary, e.hire_date, d.department_name
+        SELECT e.first_name, e.last_name, e.employee_id, e.department_id, e.phone_number, e.salary, e.hire_date, d.department_name, j.job_title
         FROM employees e
         JOIN departments d ON e.department_id = d.department_id
+        JOIN jobs j ON e.job_id = j.job_id
         WHERE e.employee_id = ? """
 
     resultado = db.execute(consulta, (id,))
